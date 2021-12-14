727a4f9efc766374b9e32981c4797418318511e6
UniXXer <unixxer@gmail.com>
2021-12-14 21:33:26 +0100

    prepare day 14

---
+++ day-14/src/test/java/rocks/unixxer/day14/TheDayTest.java
@@ -0,0 +1,28 @@
+package rocks.unixxer.day14;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.FileNotFoundException;
+
+import javax.inject.Inject;
+
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+public class TheDayTest {
+
+    @Inject
+    SolveTasks solveTasks;
+
+    @Test
+    public void testConsumer_puzzle1() throws FileNotFoundException {
+        assertEquals(0, solveTasks.solveTask1());
+    }
+
+    @Test
+    public void testConsumer_puzzle2() throws FileNotFoundException {
+        assertEquals(0, solveTasks.solveTask2());
+    }
+}
